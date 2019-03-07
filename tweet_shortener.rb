6283e143e118afa6624ca88a0346b0879b805e36
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-07 07:55:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,11 +14,4 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  tweet.split(" ").collect do |word|
-    if dictionary.keys.include?(word.downcase)
-      word = dictionary[word.downcase]
-    else
-      word 
-    end
-  end.join(" ")
-end
+  
